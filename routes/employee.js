45a04f3f705178f5910fccf80c58642721f634e6
shaghayeghzarei <sherry.zarei94@gmail.com>
2024-10-07 21:46:41 -0400

    Recoding the delete method in employee router

--- routes/employee.js
+++ routes/employee.js
@@ -83,26 +83,25 @@ router.put('/:id', async (req, res) => {
     }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/', async (req, res) => {
     try {
-        const { id } = req.params;
-        const trimmedId = id.trim();
+        const { eid } = req.query;
+        const trimmedId = eid.trim();
         if (!mongoose.Types.ObjectId.isValid(trimmedId)) {
             return res.status(400).json({ message: "Invalid Employee ID" });
         }
-        
+
         const deletedEmployee = await Employee.findByIdAndDelete(trimmedId);
 
         if (!deletedEmployee) {
             return res.status(404).json({ message: "Employee Not Found!" });
         }
 
-        res.status(200).json({  // Did not user 204 since it is "no content" and doesn't let the response to go through
-            message : "Employee Deleted successfully."
+        res.status(200).json({
+            message: "Employee Deleted successfully."
         });
-    }
-    catch (error) {
-        res.status(500).json({ message: error.message })
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 });
 
